29118911875c14a85f0dd103460f1b291c8f8694
Cee <chiennguyen2841998@gmail.com>
2025-05-23 22:05:35 +0700

    add Models folder and Stock,Comment models

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22fd8d12cd8a5774746601818952a19e0eca2598")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c60fe6fe84d1b1ded2ba803ebbb080236fefa691")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
